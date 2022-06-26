4ba14535093ef9e4cf60021864d73369f3dcba3a
Diego Sousa <diegointeriores11@gmail.com>
2022-06-26 16:06:21 -0300

    feat: add user detail endpoint

--- src/users/index.js
+++ src/users/index.js
@@ -2,6 +2,10 @@ import { getNextUsersListUrl, getUsersUrl } from "../utils/functions.js"
 import { github } from "../utils/github.js"
 
 async function getUsersList(since) {
+	if (since && typeof since !== Number) {
+		throw new Error('Since must be a number!')
+	}
+
 	const url = getUsersUrl(since)
 	const { data: users } = await github.get(url)
 	const nextUrl = getNextUsersListUrl(users)
@@ -12,6 +16,12 @@ async function getUsersList(since) {
 	}
 }
 
+async function getUser(username) {
+	const { data: user } = await github.get(`/users/${username}`)
+	return user
+}
+
 export {
-	getUsersList
+	getUsersList,
+	getUser
 }
--- src/users/router.js
+++ src/users/router.js
@@ -1,9 +1,19 @@
 import express from 'express'
-import { getUsersList } from './index.js'
+import { getUser, getUsersList } from './index.js'
 
 export const userRouter = express.Router()
 
 userRouter.get('/users', async (req, res) => {
-	const response = await getUsersList(req.query.since)
+	try {
+		const response = await getUsersList(req.query.since)
+		res.send(response)
+	} catch (error) {
+        res.status(400)
+		res.send(error.message)
+	}
+})
+
+userRouter.get('/users/:username/details', async (req, res) => {
+	const response = await getUser(req.params.username)
 	res.send(response)
 })
